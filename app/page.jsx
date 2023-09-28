4c56f1fd96526aaad672d0ad864018665d946c41
Pablo Couto <pablocouto@aluno.senai.br>
2023-09-28 08:39:00 -0300

    feat: c

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,18 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <>
-    </>
+    <main>
+      <div id="hero">
+        <h1>Aula invertida - TDS2</h1>
+        <ul>
+          <li>Pablo Couto</li>
+          <li>Daniel Santos</li>
+          <li>Caio Cicarelli</li>
+          <li>Rafael Leal</li>
+          <li>Noah</li>
+          <li>Nathan Henrique</li>
+        </ul>
+      </div>
+    </main>
   )
 }
