750d7bf98ba815fc8a239782040fc5d0b5d3b5f4
nathanhalgumacoisa <nathan.henrique@aluno.senai.br>
2023-10-03 08:49:15 -0300

    finalization of the div 'info'

--- app/page.jsx
+++ app/page.jsx
@@ -14,10 +14,15 @@ export default function Home() {
           <li>Noah</li>
           <li>Nathan Henrique</li>
         </ul>
+        <div className={styles.info}>
+          <h2>Informações sobre o projeto</h2>
+          <p>
+              Nesse projeto iremos usar mais funcionalidades do react alem de estado de variável, para adaptar
+            uma atividade feita da sprint passada para o react, sendo essa a "Game List" ou  "Locadora de jogos" .
+          </p>
+        </div>
       </div>
-      <div className={styles.info}>
-        <h2>Informações</h2>
-      </div>
+      
     </main>
   )
 }
